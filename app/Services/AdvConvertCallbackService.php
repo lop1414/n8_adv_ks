5d3ed7e9c8cc7de2654d920c699b9f6255c1649c
LinYouXin <310467466@qq.com>
2021-09-06 16:18:14 +0800

    匹配 回传

---
+++ app/Services/AdvConvertCallbackService.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Services;
+
+use App\Common\Enums\ConvertTypeEnum;
+use App\Common\Tools\CustomException;
+use App\Common\Services\ConvertCallbackService;
+
+class AdvConvertCallbackService extends ConvertCallbackService
+{
+    /**
+     * @param $item
+     * @return bool
+     * @throws CustomException
+     * 回传
+     */
+    protected function callback($item){
+        $eventTypeMap = $this->getEventTypeMap();
+
+        if(!isset($eventTypeMap[$item->convert_type])){
+            // 无映射
+            throw new CustomException([
+                'code' => 'UNDEFINED_EVENT_TYPE_MAP',
+                'message' => '未定义的事件类型映射',
+                'log' => true,
+                'data' => [
+                    'item' => $item,
+                ],
+            ]);
+        }
+
+        // 关联点击
+        if(empty($item->click)){
+            throw new CustomException([
+                'code' => 'NOT_FOUND_CONVERT_CLICK',
+                'message' => '找不到该转化对应点击',
+                'log' => true,
+                'data' => [
+                    'item' => $item,
+                ],
+            ]);
+        }
+
+        $eventType = $eventTypeMap[$item->convert_type];
+
+        //付费金额
+        $payAmount = 0;
+        if(!empty($payAmount)){
+            $payAmount =  $item->extends->amount;
+        }
+
+        $eventTime = strtotime($item->convert_at);
+        $this->runCallback($item->click,$eventType,$eventTime,$payAmount);
+
+        return true;
+    }
+
+
+
+    public function runCallback($click,$eventType,$eventTime,$payAmount = 0){
+        $url = 'http://ad.partner.gifshow.com/track/activate/';
+        $param = [
+            'event_type' => $eventType,
+            'event_time' => $eventTime
+        ];
+        if(!empty($click->link)){
+            $param['link'] = $click->link;
+        }else{
+            $param['callback'] = $click->callback_param;
+        }
+
+        if(!empty($payAmount)){
+            $param['purchase_amount'] = $payAmount;
+        }
+
+        $ret = file_get_contents($url .'?'. http_build_query($param));
+        $result = json_decode($ret, true);
+
+        if(!isset($result['code']) || $result['code'] != 0){
+            throw new CustomException([
+                'code' => 'KS_CONVERT_CALLBACK_ERROR',
+                'message' => '快手转化回传失败',
+                'log' => true,
+                'data' => [
+                    'url' => $url,
+                    'param' => $param,
+                    'result' => $result,
+                ],
+            ]);
+        }
+
+        return true;
+
+    }
+
+
+
+    /**
+     * @return array
+     * 获取事件映射
+     */
+    public function getEventTypeMap(){
+        return [
+            ConvertTypeEnum::ACTIVATION => 1,
+            ConvertTypeEnum::REGISTER => 1,
+            ConvertTypeEnum::FOLLOW => 2,
+            ConvertTypeEnum::ADD_DESKTOP => 2,
+            ConvertTypeEnum::PAY => 3,
+        ];
+    }
+
+
+
+    /**
+     * @param $click
+     * @return array|void
+     */
+    public function filterClickData($click){
+        return [
+            'id' => $click['id'],
+            'campaign_id' => $click['campaign_id'],
+            'ad_id' => $click['adgroup_id'],
+            'creative_id' => $click['creative_id'],
+            'click_at' => $click['click_at'],
+        ];
+    }
+}
---
+++ app/Services/AdvConvertMatchService.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Services;
+
+use App\Common\Enums\AdvClickSourceEnum;
+use App\Common\Enums\ConvertCallbackTimeEnum;
+use App\Common\Enums\ConvertTypeEnum;
+use App\Common\Models\ClickModel;
+use App\Common\Models\ConvertCallbackModel;
+use App\Common\Services\ConvertMatchService;
+use App\Models\Ks\KsUnitExtendModel;
+
+class AdvConvertMatchService extends ConvertMatchService
+{
+    /**
+     * @param $click
+     * @param $convert
+     * @return array|mixed|void
+     * 获取转化回传规则
+     */
+    protected function getConvertCallbackStrategy($click, $convert){
+        // 转化类型
+        $convertType = $convert['convert_type'];
+
+        // 默认策略
+        $strategy = [
+            ConvertTypeEnum::PAY => [
+                'time_range' => ConvertCallbackTimeEnum::TODAY,
+                'convert_times' => 1,
+                'callback_rate' => 100,
+                'min_amount' => 20
+            ],
+        ];
+
+        // 配置策略
+        $unitId = $click->unit_id ?? 0;
+        $unitExtend = KsUnitExtendModel::find($unitId);
+        if(!empty($unitExtend) && !empty($unitExtend->convert_callback_strategy()->enable()->first())){
+            $strategy = $unitExtend->convert_callback_strategy['extends'];
+        }
+
+        $convertStrategy = $strategy[$convertType] ?? ['time_range' => ConvertCallbackTimeEnum::NEVER];
+
+        return $convertStrategy;
+    }
+
+    /**
+     * @param $click
+     * @param $convert
+     * @return mixed
+     * 获取转化回传列表
+     */
+    protected function getConvertCallbacks($click, $convert){
+        $clickDatetime = date('Y-m-d H:i:s', strtotime("-15 days", strtotime($convert['convert_at'])));
+        $convertDate = date('Y-m-d', strtotime($convert['convert_at']));
+        $convertRange = [
+            $convertDate .' 00:00:00',
+            $convertDate .' 23:59:59',
+        ];
+
+        $unitId = $click->unit_id ?? 0;
+
+        $convertCallbackModel = new ConvertCallbackModel();
+        $convertCallbacks = $convertCallbackModel->whereRaw("
+            click_id IN (
+                SELECT id FROM clicks
+                    WHERE unit_id = '{$unitId}'
+                        AND click_at BETWEEN '{$clickDatetime}' AND '{$convert['convert_at']}'
+            ) AND convert_at BETWEEN '{$convertRange[0]}' AND '{$convertRange[1]}'
+            AND convert_type IN ('{$convert['convert_type']}')
+        ")->get();
+
+        return $convertCallbacks;
+    }
+
+    /**
+     * @param $data
+     * @return ClickModel|void
+     * 获取匹配查询构造器
+     */
+    protected function getMatchByBuilder($data){
+        $builder = new ClickModel();
+
+        if($this->clickSource != AdvClickSourceEnum::N8_TRANSFER){
+            $channelId = $data['n8_union_user']['channel_id'] ?? 0;
+            if(!empty($channelId)){
+                $builder = $builder->whereRaw("
+                unit_id IN (
+                    SELECT unit_id FROM channel_units
+                        WHERE channel_id = {$channelId}
+                )
+            ");
+            }
+        }
+
+        return $builder;
+    }
+
+
+}

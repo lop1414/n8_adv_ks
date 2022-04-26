5aab6c295bd8c98c9ae34f4a88c5791922f1adfd
linyouxin <yllobx@outlook.com>
2022-04-26 10:15:10 +0800

    素材异步任务报表

--- app/Sdks/Ks/Traits/AsyncTask.php
+++ app/Sdks/Ks/Traits/AsyncTask.php
@@ -15,7 +15,7 @@ trait AsyncTask
 
         return $this->authRequest($url, [
             'advertiser_id' => $accountId,
-            'task_name'     => "material｜{$accountId}|{$params['start_date']}",
+            'task_name'     => "material|{$accountId}|{$params['start_date']}",
             'task_params'   => $params
         ], 'POST');
     }

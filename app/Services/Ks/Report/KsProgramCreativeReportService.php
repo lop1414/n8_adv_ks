389e0bbe6acaf7b2606c6623fbc884d4b98f5cc4
linyouxin <yllobx@outlook.com>
2022-09-06 14:56:37 +0800

    定时任务：素材视频关联

--- app/Services/Ks/Report/KsProgramCreativeReportService.php
+++ app/Services/Ks/Report/KsProgramCreativeReportService.php
@@ -31,56 +31,49 @@ class KsProgramCreativeReportService extends KsReportService
         $model = new KsProgramCreativeReportModel();
         $ksMaterialData = new KsMaterialData();
         $ksVideoModel = new KsVideoModel();
-        $lastId = 0;
-        do{
-            $list = $model
-                ->where('id','>',$lastId)
-                ->whereBetween('stat_datetime', ["{$date} 00:00:00", "{$date} 23:59:59"])
-                ->groupBy('photo_id')
-                ->skip(0)
-                ->take(1000)
-                ->get();
+        $list = $model
+            ->select('photo_id','creative_id')
+            ->where('id','>',$lastId)
+            ->whereBetween('stat_datetime', ["{$date} 00:00:00", "{$date} 23:59:59"])
+            ->groupBy('photo_id','creative_id')
+            ->get();
 
-            foreach ($list as $item){
-                $lastId = $item->id;
-                $ksMaterial = $ksMaterialData->save([
-                    'material_type'    => MaterialTypeEnums::VIDEO,
-                    'file_id'          => $item->photo_id
-                ]);
+        foreach ($list as $item){
+            $ksMaterial = $ksMaterialData->save([
+                'material_type'    => MaterialTypeEnums::VIDEO,
+                'file_id'          => $item->photo_id
+            ]);
 
-                $tmpModel = new KsMaterialProgramCreativeModel();
-                $materialProgramCreative = $tmpModel
-                    ->where('material_id',$ksMaterial->id)
-                    ->where('creative_id',$item->creative_id)
-                    ->first();
-                if(!empty($materialProgramCreative)){
-                    continue;
-                }
-
-                $ksVideo = $ksVideoModel->where('id',$item->photo_id)->first();
-                if(empty($ksVideo)){
-                    var_dump("找不到视频信息：{$item->account_id} - {$item->photo_id}");
-                    continue;
-                }
-
-                $videoModel = new \App\Models\Material\VideoModel();
-                $video = $videoModel->whereRaw("
-                    (signature = '{$ksVideo->signature}' OR source_signature = '{$ksVideo->signature}')
-                ")->first();
-                $n8MaterialId = !empty($video) ? $video->id : 0;
-
-                $materialProgramCreative = $tmpModel;
-                $materialProgramCreative->material_id = $ksMaterial->id;
-                $materialProgramCreative->creative_id = $item->creative_id;
-                $materialProgramCreative->unit_id = $item->unit_id;
-                $materialProgramCreative->material_type = $ksMaterial->material_type;
-                $materialProgramCreative->n8_material_id = $n8MaterialId;
-                $materialProgramCreative->signature = $ksVideo->signature;
-                $materialProgramCreative->save();
+            $tmpModel = new KsMaterialProgramCreativeModel();
+            $materialProgramCreative = $tmpModel
+                ->where('material_id',$ksMaterial->id)
+                ->where('creative_id',$item->creative_id)
+                ->first();
+            if(!empty($materialProgramCreative)){
+                continue;
             }
 
+            $ksVideo = $ksVideoModel->where('id',$item->photo_id)->first();
+            if(empty($ksVideo)){
+                var_dump("找不到视频信息：{$item->account_id} - {$item->photo_id}");
+                continue;
+            }
 
-        }while(!$list->isEmpty());
+            $videoModel = new \App\Models\Material\VideoModel();
+            $video = $videoModel->whereRaw("
+                (signature = '{$ksVideo->signature}' OR source_signature = '{$ksVideo->signature}')
+            ")->first();
+            $n8MaterialId = !empty($video) ? $video->id : 0;
+
+            $materialProgramCreative = $tmpModel;
+            $materialProgramCreative->material_id = $ksMaterial->id;
+            $materialProgramCreative->creative_id = $item->creative_id;
+            $materialProgramCreative->unit_id = $item->unit_id;
+            $materialProgramCreative->material_type = $ksMaterial->material_type;
+            $materialProgramCreative->n8_material_id = $n8MaterialId;
+            $materialProgramCreative->signature = $ksVideo->signature;
+            $materialProgramCreative->save();
+        }
 
         $t = microtime(1) - $t;
         Functions::consoleDump($t);
